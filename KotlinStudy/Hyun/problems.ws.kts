ac389e699db9b7fa4130a76af919ba4c5c4d2c71
Kwon <105612710+Sohrw@users.noreply.github.com>
2022-09-18 09:30:54 +0900

    코틀린 학습 9월 18일 권현욱

--- KotlinStudy/Hyun/problems.ws.kts
+++ KotlinStudy/Hyun/problems.ws.kts
@@ -2,7 +2,7 @@
 var a : Int = 5
 var b : Int = 5
 //var b : Int = 7
-@@ -6,4 +7,39 @@ var c : Boolean = true
+var c : Boolean = true
 if (a!==b) {
     c = false
 }
@@ -13,6 +13,7 @@ var d : Int = 3
 var e : Int = d*2
 println(e)
 
+//problem 3
 fun score(x : Int) = when  {
     x >= 90 -> 'A'
     x >= 80 -> 'B'
@@ -24,15 +25,23 @@ println(score(85))
 println(score(70))
 println(score(60))
 
+//problem 4
 fun WriteOrWrong(x:Int) : Int {
     return x * 5
 }
 
 WriteOrWrong(10)
 
+//problem 5
 fun nullplus(x : Int? , y : Int?) : Int? {
-    if (x==null || y == null) {
-        return null
+    if (x==null && y != null) {
+        return y!!
+    }
+    else if(x != null && y == null) {
+        return x!!
+    }
+    else if(x == null && y == null) {
+        return 0
     }
     else {
         return x!! + y!!
@@ -40,5 +49,7 @@ fun nullplus(x : Int? , y : Int?) : Int? {
 
 }
 
-nullplus(3,4)
+println(nullplus(3,4))
 println(nullplus(null, 4))
+println(nullplus(3,null))
+println(nullplus(null,null))
