ab425078d11e93d7556809fe8f78cc1c6257feae
Kwon <gunhun963@gmail.com>
2022-09-22 08:44:39 +0900

    코틀린 학습 9월 22일 권현욱

---
+++ KotlinStudy/Hyun/Programmers Problems/자연수 배열로 만들어 뒤집기.ws.kts
@@ -0,0 +1,10 @@
+var n : Int = 12345
+var nToString : String = n.toString()
+var nList = nToString.chunked(1)
+var answer = mutableListOf<Int>()
+println(nList)
+
+for (i in (nList.size-1) downTo 0) {
+    answer.add(nList[i].toInt())
+}
+println(answer)
