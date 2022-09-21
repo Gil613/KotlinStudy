ecad086ca916a10182e039d7f750d69cac5b86f2
Kwon <gunhun963@gmail.com>
2022-09-22 08:27:03 +0900

    코틀린 학습 9월 22일 권현욱

---
+++ KotlinStudy/Hyun/Programmers Problems/자릿수 더하기.ws.kts
@@ -0,0 +1,9 @@
+var n : Int = 123
+var nToString : String = n.toString()
+var nArray = nToString.chunked(1)
+var answer : Int = 0
+for (i in nArray) {
+    var tInt = i.toInt()
+    answer += tInt
+}
+println(answer)
