2971db0d73a2b654033a5cb1a1a3bba94bf3d4bb
Kwon <105612710+Sohrw@users.noreply.github.com>
2022-10-06 09:03:42 +0900

    코틀린 학습 10월 6일 권현욱

--- KotlinStudy/Hyun/Programmers Problems/체육복 빌려주기.ws.kts
+++ KotlinStudy/Hyun/Programmers Problems/체육복 빌려주기.ws.kts
@@ -1,36 +1,53 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=kotlin
 
 var n : Int  = 5
-var lost = intArrayOf(2,4)
-var reserve = intArrayOf(3)
+var lost = intArrayOf(1,2,4,5)
+var reserve = intArrayOf(2,3,4)
 
 var people = mutableMapOf<Int, Int>()
 
 for( i in 1..n) {
-    if(i in lost){
-        people[i] = 0
+    if(lost.contains(i) && reserve.contains(i)){
+        people[i] = 1
     }
-    else if(i in reserve){
+    if(lost.contains(i) == false && reserve.contains(i)){
         people[i] = 2
     }
-    else {
+
+    if(lost.contains(i) == false && reserve.contains(i) == false){
         people[i] = 1
     }
+    if(lost.contains(i) && reserve.contains(i) == false) {
+        people[i] = 0
+    }
+
 }
+
 println(people)
-
-for (i in 1.. n-1) {
-    if(people[i] == 2 && people[i+1] == 0) {
-        people[i] = 1
-        people[i+1] = 1
-    }
-
+if(people[1] == 2 && people[2] == 0) {
+    people[1] = 1
+    people[2] = 1
 }
-for (i in n downTo 2) {
-    if(people[i] == 2 && people[i-1] == 0) {
-        people[i] = 1
-        people[i-1] = 1
+for (i in 2..n-1) {
+    if (people[i] == 2) {
+        if(people[i-1] == 0 && people[i+1] == 0) {
+            people[i] = 1
+            people[i-1] = 1
+        }
+        else if(people[i-1] == 0 && people[i+1] != 0) {
+            people[i] = 1
+            people[i-1] = 1
+        }
+        else if(people[i+1] == 0 && people[i-1] != 0) {
+            people[i] = 1
+            people[i+1] = 1
+        }
     }
+}
+
+if(people[n] == 2 && people[n-1] == 0) {
+    people[n] = 1
+    people[n-1] = 1
 }
 println(people)
 var answer : Int = n
