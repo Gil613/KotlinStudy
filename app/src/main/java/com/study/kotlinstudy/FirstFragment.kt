d76d6b0eeab4133282d559ced1dc60ace63b5409
gil613 <ygs3334@gmail.com>
2022-11-10 16:21:31 +0900

    Refactor for Android Mode 11/10 유길상

--- app/src/main/java/com/study/kotlinstudy/FirstFragment.kt
+++ app/src/main/java/com/study/kotlinstudy/FirstFragment.kt
@@ -6,7 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.study.kotlinstudy.databinding.FragmentFirstBinding
+import com.study.KotlinStudy.R
+import com.study.KotlinStudy.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
--- app/src/main/java/com/study/kotlinstudy/SecondFragment.kt
+++ app/src/main/java/com/study/kotlinstudy/SecondFragment.kt
@@ -6,7 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.study.kotlinstudy.databinding.FragmentSecondBinding
+import com.study.KotlinStudy.R
+import com.study.KotlinStudy.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
